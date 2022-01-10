4339be2b7e4a0ac85677a6ee87f25d678f37b987
Mem U Sins <83538724+memUsins@users.noreply.github.com>
2022-01-11 02:58:40 +0300

    Hot Fix: rename path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './_assets/css/main.scss';
+import './Assets/css/main.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
